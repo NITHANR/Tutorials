39095afbff0eba3c4d6eae9809b7afc5c8276aec
NITHANR <nithanr39@gmail.com>
2024-09-18 11:05:34 +0530

    cpmo l-5 admin

--- DJANGO/myProject/posts/views.py
+++ DJANGO/myProject/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def posts_list(request):
-    return render(request,'posts/post_list.html')
+    post_data = Post.objects.all().order_by("-date")
+    return render(request,'posts/post_list.html',{"posts":post_data})
