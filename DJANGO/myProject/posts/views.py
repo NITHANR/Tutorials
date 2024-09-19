d250606e8618c035e84ed3958f91061f85c66780
NITHANR <nithanr39@gmail.com>
2024-09-19 10:27:06 +0530

    comp l-6 slug and urls

--- DJANGO/myProject/posts/views.py
+++ DJANGO/myProject/posts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Post
+# from django.http import HttpResponse
 # Create your views here.
 def posts_list(request):
     post_data = Post.objects.all().order_by("-date")
     return render(request,'posts/post_list.html',{"posts":post_data})
+
+# def post_page(req,slug):
+#     return HttpResponse(slug)
+
+def post_page(req,slug):
+    post = Post.objects.get(slug=slug)
+    return render(req,"posts/post_page.html",{"Post":post})
