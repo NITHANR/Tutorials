2597a305b5dd08c2e7583e2c1deb03df412d40c3
NITHANR <nithanr39@gmail.com>
2024-09-24 09:18:56 +0530

    l-8 chal comp

--- DJANGO/myProject/users/urls.py
+++ DJANGO/myProject/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "users"
+
 urlpatterns = [
-    path("",views.userReg)
+    path("register/",views.register_view,name="register"),
 ]
--- DJANGO/myProject/users/views.py
+++ DJANGO/myProject/users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def userReg(req):
+def register_view(req):
     return render(req,"user/register.html")
