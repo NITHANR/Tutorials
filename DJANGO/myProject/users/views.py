c561c7effab3f5d3a2e7cb4b1197950cb45be8c2
NITHANR <nithanr39@gmail.com>
2024-09-24 10:25:25 +0530

    l-9 user reg comp

--- DJANGO/myProject/users/views.py
+++ DJANGO/myProject/users/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def register_view(req):
-    return render(req,"user/register.html")
+    if req.method == "POST":
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else:
+        form = UserCreationForm()
+    return render(req,"user/register.html",{"form":form})
