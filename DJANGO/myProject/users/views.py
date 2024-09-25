7629eba470a0747679a7e0c2290591e31622d9ca
NITHANR <nithanr39@gmail.com>
2024-09-25 09:52:38 +0530

    l-10 comp user and auth

--- DJANGO/myProject/users/views.py
+++ DJANGO/myProject/users/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login
 
 def register_view(req):
     if req.method == "POST":
         form = UserCreationForm(req.POST)
         if form.is_valid():
-            form.save()
+            login(req,form.save())
             return redirect("posts:list")
     else:
         form = UserCreationForm()
     return render(req,"user/register.html",{"form":form})
+
+def login_view(req):
+    if req.method == "POST":
+        form = AuthenticationForm(data=req.POST)
+        if form.is_valid():
+            login(req,form.get_user())
+            return redirect("posts:list")
+    else:
+        
+        form = AuthenticationForm()
+    return render(req,"user/login.html",{"form":form})
